177307b2e4cc63fd1ebb5d22a8a35e715aa8db6c
ekanurayuw <108216517+ekanurayuw@users.noreply.github.com>
2022-08-22 11:38:23 +0700

    menambahkan file dan script

---
+++ script-ubidots-eka.py
@@ -0,0 +1,128 @@
+#Libraries
+import RPi.GPIO as GPIO
+import requests
+import time
+import random
+import math
+ 
+TOKEN = "BBFF-GN4XI4vH0A4WyPPTrPPJZ8xyicSyZF"  # Put your TOKEN here
+DEVICE_LABEL = "assignment-10"  # Put your device label here
+VARIABLE_LABEL_1 = "distance"  # Put your first variable label here
+VARIABLE_LABEL_2 = "humidity"  # Put your second variable label here
+VARIABLE_LABEL_3 = "position"  # Put your second variable label here
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+
+def object_detection (distance):
+       # Jika jarak dibawah 200 cm maka status adalah bahaya
+       status = "aman"
+       if distance < 200:
+           status = "bahaya"
+       return status
+    
+def build_payload(variable_1, variable_2, variable_3):
+    # Creates two random values for sending data
+    value_1 = distance ()
+    value_2 = random.randint(0, 85)
+
+    # Creates a random gps coordinates
+    lat = random.randrange(34, 36, 1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    lng = random.randrange(-83, -87, -1) + \
+        random.randrange(1, 1000, 1) / 1000.0
+    payload = {variable_1: value_1,
+               variable_2: value_2,
+               variable_3: {"value": 1, "context": {"lat": lat, "lng": lng}}}
+
+    return payload
+    
+def main():
+    payload = build_payload(VARIABLE_LABEL_1, VARIABLE_LABEL_2, VARIABLE_LABEL_3)
+
+    print("[INFO] Attemping to send data")
+    post_request(payload)
+    print("[INFO] finished")
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(2)
+
+#kondisi
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            status = object_detection (dist)
+            print ("status adalah : ", status)
+            time.sleep(1)
+ 
+        # memberentikan code
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+
+
